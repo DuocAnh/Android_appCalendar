378815123c9c781eff514f3323c63b8c841135f7
nguyendacson <131184812+nguyendacson@users.noreply.github.com>
2024-06-23 10:24:43 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ buildscript {
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.androidApplication) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
